09de281ef9782a7c8b544ad102ffedd922462e67
Ward Driesen <ward.driesen@student.kdg.be>
2021-08-10 16:17:29 +0200

    Project initialization

---
+++ src/main/java/com/driesen/ward/mimic/MimicApplication.java
@@ -0,0 +1,13 @@
+package com.driesen.ward.mimic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/driesen/ward/mimic/MimicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driesen.ward.mimic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
